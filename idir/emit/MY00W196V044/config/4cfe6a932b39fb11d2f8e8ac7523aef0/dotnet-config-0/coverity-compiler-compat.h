fbea375b297f836200afde857f4173dbdda86082
Radzuan Najib MTAP-SER <Najib.Radzuan@mt.com>
2024-10-27 21:31:57 +0800

    init

---
+++ idir/emit/MY00W196V044/config/4cfe6a932b39fb11d2f8e8ac7523aef0/dotnet-config-0/coverity-compiler-compat.h
@@ -0,0 +1,10 @@
+﻿
+#if __COVERITY__
+#if !defined(COVERITY_COMPAT_0d54fb56a24bb8523a4c8746c0542379)
+#define COVERITY_COMPAT_0d54fb56a24bb8523a4c8746c0542379
+#pragma builtin begin
+
+
+#pragma builtin end
+#endif /* COVERITY_COMPAT_0d54fb56a24bb8523a4c8746c0542379 */
+#endif /* __COVERITY__ */
